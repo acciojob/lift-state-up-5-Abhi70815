9ad8a0fc9c737eb731f3b2127096db04d9ff085b
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-08-02 15:20:08 +0530

    sub1

--- src/components/Parents1.js
+++ src/components/Parents1.js
@@ -5,6 +5,7 @@ const Parents1 = () => {
     const [isLoggedIn,setisLoggedIn]=useState(false);
     return (
         <div>
+          <h1>Parent Component</h1>
           { isLoggedIn ? <p>You are Logged in!</p> : <Child setisLogged={setisLoggedIn}/>}
         </div>
       )
