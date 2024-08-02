7668441bf1cdbf90adb30d0480c53e0c1d796f50
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-08-02 14:59:41 +0530

    su

--- src/components/Parents1.js
+++ src/components/Parents1.js
@@ -4,11 +4,11 @@ import '../styles/Parent.css';
 const Parents1 = () => {
     const [isLoggedIn,setisLoggedIn]=useState(false);
     return (
-     <div>
-  { isLoggedIn ? <h1>You are Logged in!</h1> : <Child setisLogged={setisLoggedIn}/>}
-
-     </div>
-    )
+        <div>
+          <h1>Parent Component</h1>
+          { isLoggedIn ? <h1>You are Logged in!</h1> : <Child setisLogged={setisLoggedIn}/>}
+        </div>
+      )
 }
 
 export default Parents1
